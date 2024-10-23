0ededa67cb03f19915cecb4801a093faca558b69
Stefano Scalfari <vorshim92@gmail.com>
2024-10-24 01:43:24 +0200

    ok

---
+++ Contents/mods/Safehouse One Tile/media/lua/client/ISUI/AdminPanel/zISAddSafeZoneUI.lua
@@ -0,0 +1,7 @@
+function ISAddSafeZoneUI:updateButtons()
+	self.ok.enable = self.size > 0
+					and string.trim(self.ownerEntry:getInternalText()) ~= ""
+					and string.trim(self.titleEntry:getInternalText()) ~= ""
+					and self.notIntersecting
+					and self.character:getAccessLevel() == "Admin";
+end
