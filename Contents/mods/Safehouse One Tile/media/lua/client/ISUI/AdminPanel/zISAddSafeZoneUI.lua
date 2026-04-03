a0ed43063e8cf2b608cf48b0ff114cfa0b866eb3
Stefano Scalfari <vorshim92@gmail.com>
2026-04-04 01:58:55 +0200

    Update zISAddSafeZoneUI.lua

--- Contents/mods/Safehouse One Tile/media/lua/client/ISUI/AdminPanel/zISAddSafeZoneUI.lua
+++ Contents/mods/Safehouse One Tile/media/lua/client/ISUI/AdminPanel/zISAddSafeZoneUI.lua
@@ -5,8 +5,3 @@ function ISAddSafeZoneUI:updateButtons()
 					and self.notIntersecting
 					and self.character:getAccessLevel() == "Admin";
 end
-
-local orig_highlightZone = ISAddSafeZoneUI.highlightZone
-function ISAddSafeZoneUI:highlightZone(_x1, _x2, _y1, _y2, _fullHighlight)
-	orig_highlightZone(self, _x1, _x2 + 1, _y1, _y2 + 1, _fullHighlight)
-end
